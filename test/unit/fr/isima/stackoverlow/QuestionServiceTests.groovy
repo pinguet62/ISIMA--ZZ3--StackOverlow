5c795158166ed53da66d04073f60cecb7313363b
pinguet62 <pinguet62@gmail.com>
2013-01-23 17:32:14 +0100

    continuation test de question

--- test/unit/fr/isima/stackoverlow/QuestionServiceTests.groovy
+++ test/unit/fr/isima/stackoverlow/QuestionServiceTests.groovy
@@ -2,6 +2,8 @@ package fr.isima.stackoverlow
 
 
 
+import java.awt.print.Printable;
+
 import grails.test.mixin.*
 
 import org.junit.*
@@ -11,33 +13,58 @@ import org.junit.*
 @Mock([User,Question,Vote])
 class QuestionServiceTests {
 	
-	@Test
-	void testVoteUp() {
-		
-		Object obj;
-		Boolean res;
-		
-		User u = new User(name:'Julien', mail:'julienTheBeauGosseOf62@msn.com', password:'moimoi')
-		obj = u.save()
-		assertNotNull(obj)
-		
-		Question q = new Question(titre:"IE : de la merde, ou pire ?",
-			content:"Est-ce que Internet Explorer est bien ?",
+	
+	
+	User u
+	User u2
+	Object obj
+	Question q
+	Boolean res
+	
+	@Before 
+	public void setUp()
+	{
+		u = new User(name:'Julien', mail:'mail@mail.com', password:'moimoi')
+		u2 = new User(name:'Pierre', mail:'mailmail@mail.com', password:'mm')
+		u.save()
+		u2.save()
+		q = new Question(titre:"mytitle",
+			content:"mycontent",
 			date:new Date(),
 			user: u)
-		obj = q.save()
-		assertNotNull(obj)
-		
+		q.save()
+	}
+	
+	
+	@Test
+	void testVoteUp() {
 		MessageVotableService mServ = new MessageVotableService()
 		res = mServ.voteUp(u, q)
 		assertTrue(res)
+		
+		res = mServ.voteUp(u, q)
+		assertFalse(res)
 	}
 	
 	
 	@Test
 	void testGetMark()
 	{
+		int nbVote = -1
+		MessageVotableService mServ = new MessageVotableService()
 		
+		nbVote= mServ.getMark(q)
+		//assertEquals(nbVote, 0)
+		print nbVote
+		mServ.voteUp(u, q)
+		nbVote= mServ.getMark(q)
+		//assertEquals(nbVote, 1)
+		print nbVote
+		
+		mServ.voteUp(u2, q)
+		nbVote= mServ.getMark(q)
+		//assertEquals(nbVote, 2)
+		print nbVote
 	}
 	
 }
