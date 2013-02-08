7553dd6b637bd2695f0633f5ecc0538fa54b93ba
Arnold Pierre <pimouss_@hotmail.com>
2013-02-08 17:11:44 +0100

    commentaire ok

--- grails-app/services/fr/isima/stackoverlow/UserService.groovy
+++ grails-app/services/fr/isima/stackoverlow/UserService.groovy
@@ -3,25 +3,43 @@ package fr.isima.stackoverlow
 import fr.isima.stackoverlow.ServiceException
 
 /**
- * Gestion des utilisateurs
- * @author Julien
+ * manage the users
+ * @author Pierre
  */
 class UserService {
 	
 	/**
-	 * Existant
-	 * @param user Utilisateur
-	 * @return Vrai ou Faux
+	 * Exists 
+	 * @param user the user
+	 * @return true if user exist in database, false otherwise
 	 */
 	def exists(User user) {
 		return User.findById(user.id) != null
 	}
 	
+	/**
+	 * This methode return all the administrator from the database
+	 * @return a list of administrator
+	 */
+	def getAdmin()
+	{
+		User.findAllByAdmin(true)
+	}
 	
 	/**
-	 * Nom disponible
-	 * @param name Nom d'utilisateur
-	 * @return Vrai ou Faux
+	 * This methode return all the users of the database
+	 * @return a list of user
+	 */
+	def getAll()
+	{
+		return User.list();
+	}
+	
+	
+	/**
+	 * Disponible
+	 * @param name the name to check
+	 * @return true if the name is already used false otherwise
 	 */
 	def disponible(String name) {
 		return User.findAllByName(name).size() == 0
@@ -29,29 +47,29 @@ class UserService {
 	
 	
 	/**
-	 * Création
-	 * @param user Utilisateur
-	 * @exception IllegalArgumentException Utilisateur déjà existant
-	 * @exception ServiceException Echec de la création
+	 * create
+	 * @param user the user to add in database
+	 * @exception IllegalArgumentException user already exist
+	 * @exception ServiceException if can not save user
 	 */
     def create(User user) {
-		// Test
 		if (! disponible(user.name))
 			throw new IllegalArgumentException("Utilisateur déjà existant")
 		
 		def obj = user.save()
 		
-		// Echec
+		//check if the user have been saved
 		if (obj == null)
 			throw new ServiceException("Echec de la création")
     }
 	
 	
 	/**
-	 * Mettre à jour
-	 * @param user Utilisateur
-	 * @exception IllegalArgumentException Utilisateur innexistant
-	 * @exception ServiceException Echec de la mise à jour
+	 * Update
+	 * this method update an user
+	 * @param user the user to update
+	 * @exception IllegalArgumentException user unknow
+	 * @exception ServiceException if can not update the user
 	 */
 	def update(User user) {
 		// Test
@@ -67,27 +85,27 @@ class UserService {
 	
 	
 	/**
-	 * Supprimer
-	 * @param user Utilisateur
-	 * @exception IllegalArgumentException Utilisateur innexistant
-	 * @exception ServiceException Echec de la suppression
+	 * delete
+	 * this methode delete an user
+	 * @param user user to delete
+	 * @exception IllegalArgumentException unknow user
+	 * @exception ServiceException can not delete the user
 	 */
 	def delete(User user) {
 		
-		//delete comment
+		//delete comment about user question and answer
 		for (Commentaire com : Commentaire.list())
 		{
 			if(com.messageVotable.author.id == user.id)
 			{
-				com.delete();
+				com.delete()
 			}
 				
 		}
 		
-		//delete enswers
+		//delete answer about user question
 		for (Response response : Response.list()) 
 		{
-
 			if(response.question.author.id == user.id)
 			{
 				//coment of answer
@@ -102,29 +120,31 @@ class UserService {
 			}
 				
 		}
-		//destruction of user question.
-		List<Message> messages = Message.findAllByAuthor(user);
-		for (Message m : messages) {
+		
+		//delete of user message and their vote
+		List<Message> messages = Message.findAllByAuthor(user)
+		for (Message m : messages) 
+		{
 			for (Vote v : Vote.findAllByMessageVotable(m)) 
 			{
 				v.delete()	
 			}
-			
 			m.delete()
 		}
 		
-		//delete user
+		//delete the user
 		user.delete()
-		
-	
 	}
 	
 	
-	
+	/**
+	 * GetUserWithMark
+	 * get a list of user with a reputation diferent from 0
+	 * @return a list of user
+	 */
 	def getUserWithMark()
 	{
 		def users= User.list()
-		
 		List<User> lst = new ArrayList<User>()
 		VoteService vserv = new VoteService()
 		
@@ -136,19 +156,19 @@ class UserService {
 				lst.add(user)
 			}	
 		}
-		
-		
 		return lst
-	
 	}
 	
-	
+	/**
+	 * getUserActivity
+	 * This method return a map of message from a specified user, indexed on date
+	 * @param user to get the activity
+	 * @return
+	 */
 	def getUserActivity(User u)
 	{
 		Map<Date,List<Message>> mapRet = new HashMap<Date,List<Message>>()
-		
 		MessageService Serv = new MessageService()
-		
 		Message.findAllByAuthor(u).each 
 		{
 			if(!mapRet.containsKey(it.date))
@@ -159,5 +179,4 @@ class UserService {
 		}
 		return mapRet
 	}
-	
 }
